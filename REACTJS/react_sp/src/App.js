34374139475cff46d58684e61d6368f14bfc3103
SPatraGit <snehasisbubai2005@gmail.com>
2026-04-03 09:14:48 +0530

    React Learning

--- REACTJS/react_sp/src/App.js
+++ REACTJS/react_sp/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+let a = 10;
+let b = 20;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with me
-        </a>
-      </header>
+    <>
+    <h1>Hi This Is a Rect</h1>
+    <h2>Sum is {a+b}</h2>
+    <div className="nav">
+      <li className="name">Home</li>
+      <li className="name">About</li>
+      <li className="name">Contact</li>
     </div>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores odio vel, fuga nihil cum debitis autem dignissimos. Quos facilis voluptatem saepe. Ratione temporibus sunt odio vero rem laboriosam cum quis similique, debitis repellat molestiae voluptatem quae nulla quibusdam rerum dolores voluptatum quaerat. Ea officiis quo aliquam, veritatis eos itaque debitis vitae dolores, perferendis odio quaerat in nam odit assumenda totam reprehenderit quia voluptates cum nobis amet deserunt, nostrum laborum repellendus aperiam. In, ipsam atque, maiores voluptas omnis voluptates, esse tenetur quasi fugit corrupti laudantium ab? Consequatur eligendi adipisci nulla sed ipsa aut facilis accusantium! Excepturi odio quos fugiat ullam tempora.</p>
+    </>
   );
 }
 
