251fc0aa0a364b64ac55147b23e799ac322f03cc
SPatraGit <snehasisbubai2005@gmail.com>
2026-04-04 15:49:24 +0530

    React App Making

--- REACTJS/react_sp/src/App.js
+++ REACTJS/react_sp/src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import About from './Components/About';
 
 function App() {
   return (
     <>
-    <Navbar title = "Bubai" contactText = "Contact Us"/>
+    <Navbar title = "TextUtils" contactText = "Contact Us"/>
     {/*<Navbar/>*/}
-    <TextForm/>
+    {/*<div className="container my-3">
+      <TextForm heading = "Enter Your text"/>
+    </div>*/}
+    <div className="container my-3">
+      <About/>
+    </div>
     </>
   );
 }
--- REACTJS/react_sp/src/Components/TextForm.js
+++ REACTJS/react_sp/src/Components/TextForm.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function TextForm() {
+export default function TextForm(props) {
+    const changeTextUc = () => {
+        let newText = text.toLocaleUpperCase();
+        setText(newText)
+    }
+    const changeTextLc = () => {
+        let newText = text.toLocaleLowerCase();
+        setText(newText)
+    }
+
+    const clearText = () => {
+        let newText = '';
+        setText(newText)
+    }
+
+     const handelText = (event) => {
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState('');
     return (
-        <div>
-            <div class="mb-3">
-                <label for="myBox" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="myBox" rows="3"></textarea>
+        <>
+        <div className='container'>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" value={text} onChange={handelText}  id="myBox" rows="10"></textarea>
             </div>
+            <button className="btn btn-primary mx-1" onClick={changeTextUc}>Change_UpperCase</button>
+            <button className="btn btn-success mx-1" onClick={changeTextLc}>Change_LowerCase</button>
+            <button className="btn btn-danger mx-5" onClick={clearText}>Clear</button>
         </div>
+        <div className="container my-3">
+            <h2>Your Text Summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes for read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
