33e0c79b88a329c7dc65a5ba1b8b294f61a2df51
SPatraGit <snehasisbubai2005@gmail.com>
2026-04-04 12:39:33 +0530

    React Journey

--- REACTJS/react_sp/src/App.js
+++ REACTJS/react_sp/src/App.js
@@ -1,19 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
-let a = 10;
-let b = 20;
 function App() {
   return (
     <>
-    <h1>Hi This Is a Rect</h1>
-    <h2>Sum is {a+b}</h2>
-    <div className="nav">
-      <li className="name">Home</li>
-      <li className="name">About</li>
-      <li className="name">Contact</li>
-    </div>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores odio vel, fuga nihil cum debitis autem dignissimos. Quos facilis voluptatem saepe. Ratione temporibus sunt odio vero rem laboriosam cum quis similique, debitis repellat molestiae voluptatem quae nulla quibusdam rerum dolores voluptatum quaerat. Ea officiis quo aliquam, veritatis eos itaque debitis vitae dolores, perferendis odio quaerat in nam odit assumenda totam reprehenderit quia voluptates cum nobis amet deserunt, nostrum laborum repellendus aperiam. In, ipsam atque, maiores voluptas omnis voluptates, esse tenetur quasi fugit corrupti laudantium ab? Consequatur eligendi adipisci nulla sed ipsa aut facilis accusantium! Excepturi odio quos fugiat ullam tempora.</p>
+    <Navbar title = "Bubai" contactText = "Contact Us"/>
+    {/*<Navbar/>*/}
+    <TextForm/>
     </>
   );
 }
