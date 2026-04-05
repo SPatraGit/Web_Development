2e51016d651e104a3aed44067d14e383cc487ced
SPatraGit <snehasisbubai2005@gmail.com>
2026-04-05 17:21:12 +0530

    News app making

--- REACTJS/news_app/src/NewsItem.jsx
+++ REACTJS/news_app/src/NewsItem.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+   
+
   render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
-        This is a News Item
+      <div className='my-3'>
+        <div className="card" style={{width: "18rem"}}>
+            <img src={!imageUrl?"https://federalnewsnetwork.com/wp-content/uploads/2026/04/Trump_26091690155976-scaled.jpg":imageUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{description}...</p>
+                <a rel="noreferrer" href={newsUrl} target='blank' className="btn btn-sm btn-primary">Read more</a>
+            </div>
+            </div>
       </div>
     )
   }
--- REACTJS/news_app/src/components/News.jsx
+++ REACTJS/news_app/src/components/News.jsx
@@ -3,14 +3,71 @@ import NewsItem from '../NewsItem';
 
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-        This is a news component
-        <NewsItem/>
-      </div>
-    )
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            articles : [],
+            loading : false,
+            page : 1
+        }
+    }
+
+
+    async componentDidMount(){
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=06ce9556833a49ab83bac0aa7fc27626&page=${this.state.page+1}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({articles : parsedData.articles})
+    }
+
+
+    handlePreviousClick = async () => {
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=06ce9556833a49ab83bac0aa7fc27626&page=${this.state.page-1}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({})
+        this.setState({
+            page: this.state.page-1,
+            articles : parsedData.articles
+        })
+
+    }
+    handleNextClick = async () => {
+        console.log("Next");
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=06ce9556833a49ab83bac0aa7fc27626&page=${this.state.page+1}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({})
+        this.setState({
+            page: this.state.page+1,
+            articles : parsedData.articles
+        })
+    }
+
+    render() {
+        return (
+            <div className="container my-3">
+                <h1>ApnaNews - Top Headlines</h1>
+                
+                <div className="row">
+                    {this.state.articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                        <NewsItem  title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl = {element.url}/>
+                    </div>
+                    })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled = {this.page.state<1} type="button" class="btn btn-success" onClick={this.handlePreviousClick}> &larr; Previous</button>
+                    <button type="button" class="btn btn-success" onClick={this.handleNextClick}> Next &rarr; </button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
