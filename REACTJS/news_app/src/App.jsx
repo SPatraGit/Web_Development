ad392bb394017e1a2a0fb2299c42c6c55b874056
SPatraGit <snehasisbubai2005@gmail.com>
2026-04-05 17:59:43 +0530

    New Changes

--- REACTJS/news_app/src/App.jsx
+++ REACTJS/news_app/src/App.jsx
@@ -12,6 +12,6 @@ export default class App extends Component {
         <Navbar/>
         <News/>
       </div>
-    )
+    );
   }
 }
--- REACTJS/news_app/src/components/News.jsx
+++ REACTJS/news_app/src/components/News.jsx
@@ -10,22 +10,22 @@ export class News extends Component {
             articles : [],
             loading : false,
             page : 1
-        }
+        };
     }
 
 
     async componentDidMount(){
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=06ce9556833a49ab83bac0aa7fc27626&page=${this.state.page+1}`;
+        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=06ce9556833a49ab83bac0aa7fc27626&page=1&pageSize=20";
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
-        this.setState({articles : parsedData.articles})
+        this.setState({articles : parsedData.articles, totalResults: parsedData.totalResults})
     }
 
 
     handlePreviousClick = async () => {
         console.log("Previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=06ce9556833a49ab83bac0aa7fc27626&page=${this.state.page-1}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=06ce9556833a49ab83bac0aa7fc27626&page=${this.state.page-1}&pageSize=20`;
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
@@ -38,15 +38,20 @@ export class News extends Component {
     }
     handleNextClick = async () => {
         console.log("Next");
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=06ce9556833a49ab83bac0aa7fc27626&page=${this.state.page+1}`;
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        console.log(parsedData);
-        this.setState({})
-        this.setState({
-            page: this.state.page+1,
-            articles : parsedData.articles
-        })
+
+        if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){
+
+        }else{
+            let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=06ce9556833a49ab83bac0aa7fc27626&page=${this.state.page+1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData);
+            this.setState({})
+            this.setState({
+                page: this.state.page+1,
+                articles : parsedData.articles
+            })
+        }
     }
 
     render() {
@@ -62,8 +67,8 @@ export class News extends Component {
                     })}
                 </div>
                 <div className="container d-flex justify-content-between">
-                    <button disabled = {this.page.state<1} type="button" class="btn btn-success" onClick={this.handlePreviousClick}> &larr; Previous</button>
-                    <button type="button" class="btn btn-success" onClick={this.handleNextClick}> Next &rarr; </button>
+                    <button disabled = {this.state.page<1} type="button" className="btn btn-success" onClick={this.handlePreviousClick}> &larr; Previous</button>
+                    <button type="button" className="btn btn-success" onClick={this.handleNextClick}> Next &rarr; </button>
                 </div>
             </div>
         )
