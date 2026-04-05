2217ad7837fd9a4808d7866b2d20ad3f22c004a7
SPatraGit <snehasisbubai2005@gmail.com>
2026-04-05 18:21:42 +0530

    Now one

--- REACTJS/news_app/src/components/News.jsx
+++ REACTJS/news_app/src/components/News.jsx
@@ -57,7 +57,7 @@ export class News extends Component {
     render() {
         return (
             <div className="container my-3">
-                <h1>ApnaNews - Top Headlines</h1>
+                <div className="text-center"><h1>ApnaNews - Top Headlines</h1></div>
                 
                 <div className="row">
                     {this.state.articles.map((element) => {
@@ -67,8 +67,8 @@ export class News extends Component {
                     })}
                 </div>
                 <div className="container d-flex justify-content-between">
-                    <button disabled = {this.state.page<1} type="button" className="btn btn-success" onClick={this.handlePreviousClick}> &larr; Previous</button>
-                    <button type="button" className="btn btn-success" onClick={this.handleNextClick}> Next &rarr; </button>
+                    <button disabled = {this.state.page<=1} type="button" className="btn btn-success" onClick={this.handlePreviousClick}> &larr; Previous</button>
+                    <button disabled = {this.state.page+1 > Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-success" onClick={this.handleNextClick}> Next &rarr; </button>
                 </div>
             </div>
         )
