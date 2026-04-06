2949b46c68918ac25567a02c0acecd69c2999149
SPatraGit <snehasisbubai2005@gmail.com>
2026-04-06 09:28:21 +0530

    Something New

--- REACTJS/news_app/src/App.jsx
+++ REACTJS/news_app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 export default class App extends Component {
@@ -9,8 +10,19 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize = {8} country = "us" category = "Entertainment"/>
+        <BrowserRouter>
+          <Navbar/>
+          <Routes>
+            <Route exact path="/" element={<News key="general" pageSize = {5} country = "us" category = "General"/>} />
+            <Route exact path="/business" element={<News key="business" pageSize = {5} country = "us" category = "Business"/>} />
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize = {5} country = "us" category = "Entertainment"/>} />
+            <Route exact path="/general" element={<News key="general" pageSize = {5} country = "us" category = "General"/>} />
+            <Route exact path="/health" element={<News key="health" pageSize = {5} country = "us" category = "Health"/>} />
+            <Route exact path="/science" element={<News key="science" pageSize = {5} country = "us" category = "Science"/>} />
+            <Route exact path="/technology" element={<News key="technology" pageSize = {5} country = "us" category = "Technology"/>} />
+            <Route exact path="/sports" element={<News key="sports" pageSize = {5} country = "us" category = "Sports"/>} />
+          </Routes>
+      </BrowserRouter>
       </div>
     );
   }
--- REACTJS/news_app/src/NewsItem.jsx
+++ REACTJS/news_app/src/NewsItem.jsx
@@ -4,14 +4,18 @@ export class NewsItem extends Component {
    
 
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div className='my-3'>
         <div className="card">
-            <img src={!imageUrl?"https://planetary.s3.amazonaws.com/web/assets/pictures/_1200x630_crop_center-center_82_none/TPS-logo.png?mtime=1753477397":imageUrl} className="card-img-top" alt="..."/>
+           <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left: '50%',zIndex: '1'}}>
+              {source}
+            </span>
+            <img src={imageUrl?imageUrl:"https://planetary.s3.amazonaws.com/web/assets/pictures/_1200x630_crop_center-center_82_none/TPS-logo.png?mtime=1753477397"} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{title}...</h5>
-                <p className="card-text">{description}...</p>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <p className="card-text"><small className="text-body-secondary">By {author?author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
                 <a rel="noreferrer" href={newsUrl} target='blank' className="btn btn-sm btn-primary">Read more</a>
             </div>
             </div>
